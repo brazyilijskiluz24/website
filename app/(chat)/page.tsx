7fd493f08e29e748c314d06737f3c607abc980eb
Kamil <moskalakamil07@gmail.com>
2024-09-28 21:26:58 +0200

    fix: add missing prop

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -4,15 +4,21 @@ import { AI } from '@/lib/chat/actions'
 import { auth } from '@/auth'
 import { Session } from '@/lib/types'
 import { getMissingKeys } from '@/app/actions'
+import { Params } from '@/app/layout'
 
-export default async function IndexPage() {
+export default async function IndexPage({ params }: Params) {
   const id = nanoid()
   const session = (await auth()) as Session
   const missingKeys = await getMissingKeys()
 
   return (
     <AI initialAIState={{ chatId: id, messages: [] }}>
-      <Chat id={id} session={session} missingKeys={missingKeys} />
+      <Chat
+        lng={params.lng}
+        id={id}
+        session={session}
+        missingKeys={missingKeys}
+      />
     </AI>
   )
 }
