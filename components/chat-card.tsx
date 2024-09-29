5b647f6f29662fa11f7de7fd53cfd4980c779d0d
Kamil <moskalakamil07@gmail.com>
2024-09-29 07:41:27 +0200

    cleanup

--- components/chat-card.tsx
+++ components/chat-card.tsx
@@ -12,7 +12,7 @@ const ChatCard = (props: ChatCardProps) => {
   return (
     <div
       key={props.heading}
-      className={`cursor-pointer justify-end max-w-[18rem] min-h-[6rem] h-full rounded-lg border bg-white p-4 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900 flex flex-col items-center w-full`}
+      className={`cursor-pointer justify-end max-w-[20rem] min-h-[6rem] h-full rounded-lg border bg-white p-4 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900 flex flex-col items-center w-full`}
       onClick={props.onClick}
     >
       <img
